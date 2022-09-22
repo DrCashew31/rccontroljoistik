ece9fdd2a1546f952a5cf1a1298e8b05f4010f91
DrCashew31 <113105239+DrCashew31@users.noreply.github.com>
2022-09-22 15:48:50 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,6 +12,8 @@ basic.forever(function () {
         radio.sendNumber(5)
     } else if (ModuleWorld_Analog.Rocker(ModuleWorld_Analog.mwAnalogNum.AP0P1, ModuleWorld_Analog.enRocker.NoState)) {
         radio.sendNumber(5)
+    } else if (ModuleWorld_Analog.Rocker(ModuleWorld_Analog.mwAnalogNum.AP2P3, ModuleWorld_Analog.enRocker.Up)) {
+        radio.sendNumber(6)
     } else {
     	
     }
